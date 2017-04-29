0fa2561f5df744078c5b71b56ec7abd3ee03852a
Yat3s <4645643@qq.com>
2017-04-29 21:19:09 +0800

    Updated admin config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ TULING_KEY = "c697910a18b540d4a63838871ee9a671"
 DEBUG = True
 NAME = 'Aran'
 AVATAR = 'http://img.67.com/upload/images/2016/05/26/aGV5YW96aG91MTQ2NDI0Njk3NQ==.jpg'
-ADMIN_NAME = u'echo小萌'
+ADMIN_NAME = u'Yat3s'
 
 SEX_PIC_URL = [\
 'http://i4.buimg.com/4851/5388a44f9afe566e.jpg', \
