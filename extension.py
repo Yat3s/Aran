4e734571ac49b6b3d45f7628b2f418e9070f10e5
Yat3s <4645643@qq.com>
2017-05-03 16:12:24 +0800

    Removed jnlu

--- extension.py
+++ extension.py
@@ -8,57 +8,27 @@ from scrapy import *
 lastSequenceId = ''
 def auto_reply(msg, uid):
     if TULING_KEY:
-        # url = "http://www.tuling123.com/openapi/api"
-        # user_id = uid.replace('@', '')[:30]
-        # body = {'key': TULING_KEY, 'info': msg.encode('utf8'), 'userid': user_id}
-        # r = requests.post(url, data = body)
-        # respond = json.loads(r.text)
-        # result = ''
-        # code = respond['code']
-        # text = respond['text']
-        # if code == 100000: # TEXT
-        #     result = text
-        # elif code == 200000: # URL
-        #     result = text + '\n' + respond['url']
-        # elif code == 302000: # News list
-        #     for item in respond['list']:
-        #         result += u'【'+ item['source'] + u'】' + item['article'] + '\n' + item['detailurl'] + '\n\n'
-        # elif code == 308000: # Cook menu
-        #     for item in respond['list']:
-        #         result += u'【'+ item['name'] + u'】' + item['info'] + '\n' + item['detailurl'] + '\n\n'
-        # return result
-        global lastSequenceId
-        if msg.isdigit() :
-            if int(msg) > 0 and int(msg) < 6:
-                return score(msg, lastSequenceId)
-            else:
-                return u'你评的这个分有点飘吧....'
-        else:
-            url = "http://jnlu.jd.com/jnlu/getAiNlu.ajax"
-            params = {'sessionId': lastSequenceId, 'inputText' : msg.encode('utf8')}
-            headers = {'Cookie': 'sso.jd.com=39c9b9883e2545aea9c5ed5792dcde83;'}
-            r = requests.post(url, data = params, headers = headers)
-            respond = json.loads(r.text)
-            result = msg
-            data = respond['data']
-            if data:
-                type = data['type']
-                lastSequenceId = data['sequenceId']
-                if type == 'TYPE_STRING':
-                    result = data['responses']['string']
-                elif type == 'TYPE_MEDIA':
-                    result = u'[Media]' + data['responses']['string'] + '\n' + data['responses']['media']
-                return result
+        url = "http://www.tuling123.com/openapi/api"
+        user_id = uid.replace('@', '')[:30]
+        body = {'key': TULING_KEY, 'info': msg.encode('utf8'), 'userid': user_id}
+        r = requests.post(url, data = body)
+        respond = json.loads(r.text)
+        result = ''
+        code = respond['code']
+        text = respond['text']
+        if code == 100000: # TEXT
+            result = text
+        elif code == 200000: # URL
+            result = text + '\n' + respond['url']
+        elif code == 302000: # News list
+            for item in respond['list']:
+                result += u'【'+ item['source'] + u'】' + item['article'] + '\n' + item['detailurl'] + '\n\n'
+        elif code == 308000: # Cook menu
+            for item in respond['list']:
+                result += u'【'+ item['name'] + u'】' + item['info'] + '\n' + item['detailurl'] + '\n\n'
+        return result
     else:
         return u'我知道啦'
-
-def score(score, id):
-    url = "http://jnlu.jd.com/jnlu/userScore.ajax"
-    params = {'sequenceId': id, 'score' : score}
-    headers = {'Cookie': 'sso.jd.com=39c9b9883e2545aea9c5ed5792dcde83;'}
-    r = requests.post(url, data = params, headers = headers)
-    respond = json.loads(r.text)
-    return (u'么么哒~ 谢谢你的评分(' + score + u')，请拿好你的小票：\n' + id) if respond['success'] else u'评分失败'
 
 ## return: if True consume this action.
 def process_command(content, from_user_id, from_user_name):
